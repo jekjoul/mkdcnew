106ccc1c1e852b564bb252048bd290867cdab69a
jekjoul <jekjoul@gmail.com>
2025-09-09 10:00:39 +0700

    Perbaikan nav dashboard

--- application/controllers/Dashboard.php
+++ application/controllers/Dashboard.php
@@ -11,6 +11,7 @@ class Dashboard extends MY_Controller {
 	public function index()
 	{
 		$this->page_data['page']->title = 'Dashboard';
+		$this->page_data['page']->titleUrl = 'dashboard';
 		$this->page_data['page']->icon = 'solar:home-angle-2-linear';
 		
 		$this->load->view('dashboard', $this->page_data);
