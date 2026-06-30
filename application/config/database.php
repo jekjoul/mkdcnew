c005b5b83968b6893e817fe83a425e5e49289b07
jekjoul <jekjoul@gmail.com>
2026-07-01 02:38:12 +0700

    update db server

--- application/config/database.php
+++ application/config/database.php
@@ -1,5 +1,5 @@
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
+defined('BASEPATH') or exit('No direct script access allowed');
 
 /*
 | -------------------------------------------------------------------
@@ -76,9 +76,9 @@ $query_builder = TRUE;
 $db['default'] = array(
 	'dsn'	=> '',
 	'hostname' => 'localhost',
-	'username' => 'root',
-	'password' => '',
-	'database' => 'mkdcnew',
+	'username' => 'mifx4818_mkdcnew',
+	'password' => 'Miftahulkhoer100%',
+	'database' => 'mifx4818_mkdcnew',
 	'dbdriver' => 'mysqli',
 	'dbprefix' => '',
 	'pconnect' => FALSE,
