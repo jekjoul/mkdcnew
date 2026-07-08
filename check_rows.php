73dc19e01a1eae35f42d7358ff72cf36f6008410
jekjoul <jekjoul@gmail.com>
2026-07-08 16:08:21 +0700

    integrasi google drive

--- check_rows.php
+++
@@ -1,6 +0,0 @@
-<?php
-$conn = new mysqli('localhost', 'root', '', 'mkdcnew');
-$res = $conn->query("SELECT * FROM jadwal_pelajaran_pengaturan");
-while ($row = $res->fetch_assoc()) {
-    print_r($row);
-}
